4a8db4af64ecc228b17e2ac99cfd5e2801dbe7d6
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-26 16:52:43 +0100

    Add compare logic to PokerHand class

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 let Result = { "win": 1, "loss": 2, "tie": 3 };
 
+
 class PokerHand {
 
 
@@ -8,7 +9,13 @@ class PokerHand {
 	}
 
 	compareWith(hand){
-		return Result.tie
+		let handArray = hand.split(' ')
+		let hand1 = Hand.solve(this.handOne)
+		let hand2 = Hand.solve(handArray)
+		if (hand1.rank == hand2.rank) return Result.tie
+		return (hand1.rank > hand2.rank) ? Result.win : Result.loss
+
+
 	}
 
 }
